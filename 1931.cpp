6df54c87f04ed22f14751fe33366e8b2197e4344
Danijoa <danijoa1020@gmail.com>
2020-10-28 23:18:47 +0900

    회의실배정

---
+++ 1931.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <deque>
+#define MAX 100000
+using namespace std;
+
+int num;
+deque<pair<int, int>> myQ;
+int endT;
+int answer = 0;
+
+int main()
+{
+	cin >> num;
+
+	int a, b;
+	for (int i = 0; i < num; i++)
+	{
+		cin >> a >> b;
+		myQ.push_back(pair<int, int>(b, a));
+	}
+
+	//끝나는 시간 기준 정렬
+	sort(myQ.begin(), myQ.end());
+
+	endT = myQ.front().first;
+	myQ.pop_front();
+	answer++;
+	while (myQ.empty() != true)
+	{
+		if (endT <= myQ.front().second)
+		{
+			answer++;
+			endT = myQ.front().first;
+		}
+		myQ.pop_front();
+	}
+
+	cout << answer;
+}
