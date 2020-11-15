1b53bc258eae6d850f1af808683a9395646150e3
Danijoa <danijoa1020@gmail.com>
2020-11-15 15:15:19 +0900

    Divisors

--- 13255.cpp
+++ 13255.cpp
@@ -1,27 +1,19 @@
 #include <iostream>
-#include <math.h>
 using namespace std;
 
 int c;	//약수 개수 구할 숫자 개수 1<=c<=10
 int n;	//숫자 1<=n<=10000
-int a; 	//약수 개수
+int d;	// 약수들 1<=d<=n
 
 void solution(int n)
 {
 	a = 0;
 
-	for (int i = 1; i <= sqrt(n); i++)
+	for (int i = 1; i <= n; i++)
 	{
-		if (n % i == 0)
+		if (n%i==0)
 		{
-			if (i == sqrt(n))
-			{
-				a += 1;
-			}
-			else
-			{
-				a += 2;
-			}
+			a++;
 		}
 	}
 
