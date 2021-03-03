ce77cac40ca16c984fc230b2691388985b5fceab
Danijoa <danijoa1020@gmail.com>
2021-03-03 15:21:27 +0900

    미로 탐색

--- 2178.cpp
+++ 2178.cpp
@@ -1,77 +1,61 @@
 #include <iostream>
 #include <queue>
+#define MAX 101
 using namespace std;
 
-int n, m;   //행렬
-int** myMetrix;
-int** visited; //방문 확인 및 1에서 몇번째 자식 확인
+int garo, searo;
+int maze[MAX][MAX];
+int visited[MAX][MAX];   //방문했으면 1 안했으면 0
+queue <pair<int, int>> dataQ;  //(cost, (i, j))
+int dX[4] = { -1,1,0,0 };
+int dY[4] = { 0,0,-1,1 };
 
-void BFS(int sX, int sY)
+void bfs()
 {
-	visited[sX][sY] = 1;
+    while (!dataQ.empty())
+    {
+        int curX = dataQ.front().first;
+        int curY = dataQ.front().second;
+        dataQ.pop();
 
-	queue<pair<int, int>> myQ;
-	myQ.push(make_pair(sX, sY));
+        for (int i = 0; i < 4; i++)
+        {
+            int nextX = curX + dX[i];
+            int nextY = curY + dY[i];
 
-	int dX[4] = { -1, 1, 0 , 0 };   //상하
-	int dY[4] = { 0, 0, -1, 1 };   //좌우
-	while (myQ.empty() != true)
-	{
-		int x = myQ.front().first;
-		int y = myQ.front().second;
-		myQ.pop();
-
-		for (int i = 0; i < 4; i++)
-		{
-			int tX = x + dX[i];
-			int tY = y + dY[i];
-			if ((1 <= tX && tX <= n) && (1 <= tY && tY <= m))
-			{
-				if (myMetrix[tX][tY] == 1 && visited[tX][tY] == 0)
-				{
-					myQ.push(make_pair(tX, tY));
-					visited[tX][tY] = visited[x][y] + 1;
-				}
-			}
-		}
-	}
+            if ((nextX >= 1 && nextX <= searo) && (nextY >= 1 && nextY <= garo))   //범위안에 있는
+            {
+                if (maze[nextX][nextY] == 1 && visited[nextX][nextY] == 0)  //길이면서, 아직 방문 전이면
+                {
+                    dataQ.push(make_pair(nextX, nextY));
+                    visited[nextX][nextY] = visited[curX][curY] + 1;
+                }
+            }
+        }
+    }
 }
 
 int main()
 {
-	cin >> n >> m;   //행렬
+    //입력
+    cin >> searo >> garo;
+    for (int i = 1; i <= searo; i++)
+    {
+        for (int j = 1; j <= garo; j++)
+        {
+            scanf_s("%1d", &maze[i][j]);   //붙여서 출력(0: 벽  /  1:길)
+        }
+    } 
 
-	//myMetrix 생성 및 값 입력
-	myMetrix = new int* [n + 1];
-	for (int i = 0; i < n + 1; i++)
-	{
-		myMetrix[i] = new int[m + 1];
-	}
+    //초기화
+    visited[1][1] = 1;
+    dataQ.push(make_pair(1, 1));
 
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= m; j++)
-		{
-			scanf("%1d", &myMetrix[i][j]);
-		}
-	}
+    //4가지 방향을 확인해 가며 전진하니까 bfs를 사용해 보자
+    bfs();
 
-	//visited 생성 및 초기화
-	visited = new int* [n + 1];
-	for (int i = 0; i < n + 1; i++)
-	{
-		visited[i] = new int[m + 1];
-	}
+    //결과
+    cout << visited[searo][garo];
 
-	for (int i = 1; i <= n; i++)
-	{
-		for (int j = 1; j <= m; j++)
-		{
-			visited[i][j] = 0;
-		}
-	}
-
-	BFS(1, 1);
-
-	cout << visited[n][m];
+	return 0;
 }
