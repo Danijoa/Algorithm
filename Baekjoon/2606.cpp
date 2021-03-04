c802bd76072513d2a0a89e708bb7452f8bb6c6c5
Danijoa <danijoa1020@gmail.com>
2021-03-04 23:09:04 +0900

    바이러스

--- Baekjoon/2606.cpp
+++ Baekjoon/2606.cpp
@@ -1,3 +1,4 @@
+//	BFS version
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -56,3 +57,60 @@ int main()
 
 	cout << answer;
 }
+
+/*	DFS version
+#include <iostream>
+#include <vector>
+#define MAX 101
+using namespace std;
+
+int com;
+int vertex;
+vector<int> verV[MAX];
+int visited[MAX];
+int cnt = 0;
+
+void dfs(int node)
+{
+	visited[node] = 1;
+	cnt++;
+
+	for (int i = 0; i < verV[node].size(); i++)
+	{
+		int cur = verV[node][i];	//이웃노드 확인
+		if (visited[cur] == 0)	//방문안했으면
+		{
+			dfs(cur);
+		}
+	}
+}
+
+
+int main()
+{
+	//입력
+	cin >> com;
+	cin >> vertex;
+
+	for (int i = 0; i < vertex; i++)
+	{
+		int a, b;
+		cin >> a >> b;
+
+		verV[a].push_back(b);
+		verV[b].push_back(a);
+	}
+
+	//초기화
+	for (int i = 0; i < com + 1; i++)
+		visited[i] = 0;
+
+	//dfs
+	dfs(1);
+
+	//출력
+	cout << (cnt - 1);	//첫시작 빼주기
+
+	return 0;
+}
+*/
