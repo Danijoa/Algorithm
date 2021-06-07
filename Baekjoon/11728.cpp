35ff4300baaf0c746bd42a9897ab8ef3f1b5f5aa
Danijoa <danijoa1020@gmail.com>
2021-06-08 00:36:56 +0900

    배열 합치기

--- Baekjoon/11728.cpp
+++ Baekjoon/11728.cpp
@@ -1,35 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int n, m;
+int size_1, size_2;
 int list_1[1000001];
 int list_2[1000001];
 int list_HAP[2000002];
 
-void merge()
+void sort()
 {
 	int i = 0, j = 0, k = 0;
 
-	while (i < n && j < m)
+	// 비교 삽입
+	while (i < size_1 && j < size_2)
 	{
 		if (list_1[i] <= list_2[j])
 		{
 			list_HAP[k] = list_1[i];
-			k++, i++;
+			i++;
 		}
 		else
 		{
 			list_HAP[k] = list_2[j];
-			k++, j++;
+			j++;
 		}
+		k++;
 	}
 
-	while (i < n)
+	// 남아있는 애들 삽입
+	while (i < size_1)
 	{
 		list_HAP[k] = list_1[i];
 		k++, i++;
 	}
-	while (j < m)
+	while (j < size_2)
 	{
 		list_HAP[k] = list_2[j];
 		k++, j++;
@@ -41,22 +44,18 @@ int main()
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 
-	cin >> n >> m;
+	cin >> size_1 >> size_2;
 
-	for (int i = 0; i < n; i++)
-	{
+	for (int i = 0; i < size_1; i++)
 		cin >> list_1[i];
-	}
 
-	for (int i = 0; i < m; i++)
-	{
+	for (int i = 0; i < size_2; i++)
 		cin >> list_2[i];
-	}
 
-	merge();
+	sort();
 
-	for (int i = 0; i < n + m; i++)
-	{
+	for (int i = 0; i < size_1 + size_2; i++)
 		cout << list_HAP[i] << " ";
-	}
+
+	return 0;
 }
