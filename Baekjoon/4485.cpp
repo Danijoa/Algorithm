461c025346c0a914bad1a7c58a1ea23eb011ee60
Danijoa <danijoa1020@gmail.com>
2021-06-12 23:51:30 +0900

    녹색 옷 입은 애가 젤다지?

--- Baekjoon/4485.cpp
+++ Baekjoon/4485.cpp
@@ -93,3 +93,106 @@ int main()
 
 	return 0;
 }
+/*
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#define MAX 126
+#define INF 2250
+using namespace std;
+
+typedef struct mapData
+{
+    int distance;
+    int x;
+    int y;
+}; 
+
+typedef struct cmp
+{
+    bool operator()(mapData& a, mapData& b)
+    {
+        return (a.distance > b.distance);
+    }
+};
+
+pair<int, int> target;
+int map[MAX][MAX];
+int dist[MAX][MAX];
+priority_queue<mapData, vector<mapData>, cmp> myQ;
+int dir[4][2] = { {-1, 0}, {1, 0}, {0, -1}, {0, 1} };
+
+int Dijkstra(int size, pair<int, int> target)
+{
+    // 시작 지점 저장
+    dist[0][0] = map[0][0];
+    myQ.push({ -1 * map[0][0], 0, 0 });
+
+    while (!myQ.empty())
+    {
+        int curX = myQ.top().x;
+        int curY = myQ.top().y;
+        int curDist = -1 * myQ.top().distance;
+        myQ.pop();
+
+        if (curDist > dist[curX][curY])
+            continue;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nextX = curX + dir[i][0];
+            int nextY = curY + dir[i][1];
+            int nextDist = map[nextX][nextY];
+
+            if (nextX < 0 || nextX >= size || nextY < 0 || nextY >= size)
+                continue;
+
+           if (dist[nextX][nextY] > dist[curX][curY] + nextDist)		//dist[curX][curY] curDist
+            {
+                dist[nextX][nextY] = dist[curX][curY] + nextDist;
+                myQ.push({ -1 * dist[nextX][nextY], nextX , nextY });
+            }
+        }
+    }
+
+    return dist[size - 1][size - 1];
+}
+
+int main()
+{
+    int size;
+    int cnt = 0;
+    while (true)
+    {
+        cnt++;
+
+        // 맵 크기
+        cin >> size;
+        if (size == 0)
+            break;
+
+        // 맵 셋팅
+        fill(&map[0][0], &map[size - 1][size - 1], 0);
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                cin >> map[i][j];
+            }
+        }
+
+        // 거리 갱신 저장
+        //fill(&dist[0][0], &dist[size - 1][size - 1], INF);
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                dist[i][j] = INF;
+            }
+        }
+
+        // 시작~도착 최단 거리
+        cout << "Problem " << cnt << ": " << Dijkstra(size, target);
+    }
+}
+*/
