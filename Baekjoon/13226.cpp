810721a0b94a8a8452dfaaa0b69b93910e78120d
Danijoa <danijoa1020@gmail.com>
2021-07-04 22:57:03 +0900

    Divisors Again

--- Baekjoon/13226.cpp
+++ Baekjoon/13226.cpp
@@ -1,16 +1,11 @@
 #include <iostream>
-#include <algorithm>
-#include <math.h>
-#define MAX 10000001
+#include "math.h"
 using namespace std;
 
-int c;	//약수 개수 구할 숫자 개수 1<=c<=10
-int l;	//작은 숫 (두 수 차이는 1000이하)
-int u;	//큰 수 1<=u<=10000000 
 int cnt;
 int result;
 
-void solution(int l, int u)	//l~u 사이 수 중 가장 약수의 개수가 큰 수의 약수 개수를 구하시오
+void DiviserCount(int l, int u) //l ~ u 사이 수 중 가장 약수의 개수가 큰 수의 약수 개수를 구하시오
 {
 	result = 0;
 	for (int n = l; n <= u; n++)
@@ -21,13 +16,9 @@ void solution(int l, int u)	//l~u 사이 수 중 가장 약수의 개수가 큰 
 			if (n % i == 0)
 			{
 				if (i == sqrt(n))
-				{
 					cnt += 1;
-				}
 				else
-				{
 					cnt += 2;
-				}
 			}
 		}
 		result = max(result, cnt);
@@ -37,11 +28,13 @@ void solution(int l, int u)	//l~u 사이 수 중 가장 약수의 개수가 큰 
 
 int main()
 {
-	cin >> c;
-	while (c != 0)
-	{
-		c--;
-		cin >> l >> u;
-		solution(l, u);
-	}
+    int c;
+    cin >> c;
+
+    for (int i = 0; i < c; i++)
+    {
+        int l, u;
+        cin >> l >> u;
+        DiviserCount(l, u);
+    }
 }
