737e0b4738535871eada2634a58a151e3aa5383b
Danijoa <danijoa1020@gmail.com>
2021-07-04 23:49:14 +0900

    경로 찾기

--- Baekjoon/11403.cpp
+++ Baekjoon/11403.cpp
@@ -1,89 +1,70 @@
+/*
+int* visited = new int[n];	// 방문 리스트
+int** myList;	//인접행렬
+myList = new int* [n];
+for (int i = 0; i < n; i++)
+{
+	myList[i] = new int[n];
+}
+*/
+	
 #include <iostream>
 #include <queue>
+#define MAX 102
 using namespace std;
 
-int n;	//정점개수
-int** myList;	//인접행렬
-int** answerList;	//정답행렬
+int graph[MAX][MAX];    // 가중치 없는 방향그래프
+int answer[MAX][MAX] = { 0, };
+int vertex;
 
 void BFS(int start)
 {
-	int* visited = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		visited[i] = 0;
-	}
+    bool visited[MAX] = { false, };
+    //answer[start][start] = 1;
+    //visited[start] = true;
 
-	queue<int> myQ;
-	myQ.push(start);
+    queue<int> myQ;
+    myQ.push(start);
 
-	int v;
-	while (myQ.empty() != true)
-	{
-		v = myQ.front();
-		myQ.pop();
+    while (!myQ.empty())
+    {
+        int cur = myQ.front();
+        myQ.pop();
 
-		for (int i = 0; i < n; i++)
-		{
-			if (myList[v][i] == 1 && visited[i] != 1)
-			{
-				myQ.push(i);
-				answerList[start][i] = 1;
-				visited[i] = 1;
-			}
-		}
-	}
+        for (int i = 1; i <= vertex; i++)
+        {
+            if (visited[i] == false && graph[cur][i] == 1)
+            {
+                myQ.push(i);
+                answer[start][i] = 1;
+                visited[i] = true;
+            }
+        }
+    }
+
 }
 
 int main()
 {
-	//정점 개수 입력
-	cin >> n;
+    cin >> vertex;
 
-	//myList 생성 및 초기화
-	myList = new int* [n];
-	for (int i = 0; i < n; i++)
-	{
-		myList[i] = new int[n];
-	}
+    for (int i = 1; i <= vertex; i++)
+    {
+        for (int j = 1; j <= vertex; j++)
+        {
+            cin >> graph[i][j];
+        }
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cin >> myList[i][j];
-		}
-	}
+    for (int i = 1; i <= vertex; i++)
+        BFS(i);
 
-	//answerList 생성 및 초기화
-	answerList = new int* [n];
-	for (int i = 0; i < n; i++)
-	{
-		answerList[i] = new int[n];
-	}
-
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			answerList[i][j] = 0;
-		}
-	}
-
-	//BFS수행
-	for (int i = 0; i < n; i++)
-	{
-		BFS(i);
-	}
-
-
-	//결과 출력
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			cout << answerList[i][j] << ' ';
-		}
-		cout << '\n';
-	}
+    for (int i = 1; i <= vertex; i++)
+    {
+        for (int j = 1; j <= vertex; j++)
+        {
+            cout << answer[i][j] << " ";
+        }
+        cout << "\n";
+    }
 }
