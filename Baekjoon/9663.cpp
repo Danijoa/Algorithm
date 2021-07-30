cabb2665a751456c751dec34e0578e613dcb7192
Danijoa <danijoa1020@gmail.com>
2021-07-31 00:06:32 +0900

    N-Queen

--- Baekjoon/9663.cpp
+++ Baekjoon/9663.cpp
@@ -6,7 +6,7 @@ int cnt = 0;
 
 bool check(int checkRow, int queen[])
 {
-	for (int i = 1; i <= rowSize; i++)
+	for (int i = 1; i < checkRow; i++)
 	{
 		// 같은 열에(col) 또 다른 퀸이 있다
 		// 같은 대각선에 또 다른 퀸이 있다
@@ -28,15 +28,16 @@ void backTrack(int curRow, int queen[])
 	// 놓을 퀸이 남아 있는 경우
 	else
 	{
+		// 현재 행의 +1열씩 증가해가며 퀸 놓을 자리를 확인
 		for (int i = 1; i <= rowSize; i++)
 		{
-			// (curRow + 1)행 i열에 놓아보기
+			// 일단 (curRow + 1)행 i열에 놓아보기
 			queen[curRow + 1] = i;
 			
-			// 놓을 수 있는 경우
+			// 놓을 수 있는 경우 다음 행 확인
 			if (check(curRow + 1, queen))
-				backTrack(rowSize, queen);
-			// 놓을 수 없는 경우
+				backTrack(curRow + 1, queen);
+			// 놓을 수 없는 경우 다시 복구
 			else
 				queen[curRow + 1] = 0;
 		}
