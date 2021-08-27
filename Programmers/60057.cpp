d5845b7f8f7ce91dc5efe884687e045a31723109
Danijoa <danijoa1020@gmail.com>
2021-08-27 23:49:47 +0900

    문자열 압축

---
+++ Programmers/60057.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) 
+{
+    int answer = 0;
+
+    //크기 1 ~ length/2 확인
+    for (int i = 1; i <= s.length(); i++)
+    {
+        // substr(start index, length)
+        for (int j = 0; j < s.length(); j++)
+        {
+            if(s.substr()==s())
+        }
+    }
+    
+    return answer;
+}
