5c8632d57c96d849109c8bee4d172229e3caddcd
Danijoa <danijoa1020@gmail.com>
2021-09-01 23:35:53 +0900

    직업군 추천하기

---
+++ Programmers/84325.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> table, vector<string> languages, vector<int> preference) 
+{
+    string answer = "";
+    return answer;
+}
