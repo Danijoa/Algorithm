30d3e85bf1110d1b2f25db9c476bd8e308a60a05
Danijoa <danijoa1020@gmail.com>
2021-12-14 23:55:22 +0900

    섬나라 아일랜드

---
+++ Materials/87.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int mapS;
+int map[20][20];
+bool visit[20][20] = { false, };
+int cnt = 0;
+int dirX[4] = { 0, 0, 1, -1 };
+int dirY[4] = { 1, -1, 0, 0 };
+
+void bfs(int x, int y)
+{
+    queue<pair<int, int> > myQ;
+    myQ.push(make_pair(x, y));
+    visit[x][y] = true;
+
+    while (!myQ.empty())
+    {
+        int curX = myQ.front().first;
+        int curY = myQ.front().second;
+        myQ.pop();
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nextX = curX + dirX[i];
+            int nextY = curY + dirY[i];
+
+            if (nextX < 0 || nextX >= mapS || nextY < 0 || nextY >= mapS)
+                continue;
+
+            if (visit[nextX][nextY] == false && map[nextX][nextY] == 1)
+            {
+                visit[nextX][nextY] = true;
+                myQ.push(make_pair(nextX, nextY));
+            }
+        }
+    }
+
+}
+
+int main(void)
+{    
+    cin >> mapS;
+
+    for (int i = 0; i < mapS; i++)
+        for (int j = 0; j < mapS; j++)
+            cin >> map[i][j];
+
+    for (int i = 0; i < mapS; i++)
+    {
+        for (int j = 0; j < mapS; j++)
+        {
+            if (visit[i][j] == false && map[i][j] == 1)
+            {
+                cout << i << " " << j << endl;
+                cnt += 1;
+                bfs(i, j);
+            }
+        }
+    }
+    bfs(0, 0);
+
+    cout << cnt;
+
+    return 0;
+}
